e57e790823be6c5b129ad045d6792ac92e8ae645
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-07-04 16:37:55 +0900

    feat: 공통 목록 조회 페이지를 위한 ListLayout 레이아웃 구현 (#10)

---
+++ src/components/layout/ListLayout.vue
@@ -0,0 +1,66 @@
+<template>
+  <main>
+    <!-- page-header -->
+    <section class="page-header">
+      <h1 class="page-title">{{ title }}</h1>
+      <p class="page-description">{{ description }}</p>
+    </section>
+
+    <!-- filters-section -->
+    <ListFiltersSection>
+      <slot name="filters" />
+    </ListFiltersSection>
+
+    <!-- top-actions -->
+    <ListTopActions>
+      <template #left>
+        <slot name="top-actions-left" />
+      </template>
+      <template #right>
+        <slot name="top-actions-right" />
+      </template>
+    </ListTopActions>
+
+    <!-- table-section -->
+    <ListTableSection>
+      <slot name="table" />
+    </ListTableSection>
+  </main>
+</template>
+
+<script setup>
+import ListFiltersSection from "@/components/layout/listview/ListFiltersSection.vue";
+import ListTopActions from "@/components/layout/listview/ListTopActions.vue";
+import ListTableSection from "@/components/layout/listview/ListTableSection.vue";
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+main {
+  padding: 1rem;
+  overflow-y: auto;
+}
+
+/* page-header */
+.page-title {
+  font-size: 1.5rem;
+  color: #1f2d3d;
+  margin-bottom: 10px;
+  border-left: 4px solid #3aaed8;
+  padding-left: 10px;
+}
+.page-description {
+  color: #6b7280;
+  font-size: 1rem;
+}
+</style>
