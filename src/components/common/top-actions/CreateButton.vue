2cc3908e1615d80ff5bbc501136761274a91cbbe
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-07-05 19:43:58 +0900

    feat: 버튼에 대한 공통 컴포넌트 구현 (#12)

--- src/components/common/top-actions/CreateButton.vue
+++ src/components/common/top-actions/CreateButton.vue
@@ -1,26 +1,9 @@
 <template>
-  <button class="btn btn-create" @click="$emit('click')">
+  <StatusButton type="primary" @click="$emit('click')">
     <slot>신규 등록</slot>
-  </button>
+  </StatusButton>
 </template>
 
 <script setup>
-defineEmits(['click'])
+import StatusButton from '../StatusButton.vue'
 </script>
-
-<style scoped>
-.btn-create {
-  padding: 8px 16px;
-  font-size: var(--font-button);
-  font-weight: 500;
-  border-radius: 6px;
-  cursor: pointer;
-  border: none;
-  background-color: var(--color-primary);
-  color: white;
-  transition: background-color 0.2s;
-}
-.btn-create:hover {
-  background-color: #2c9cc3;
-}
-</style>
